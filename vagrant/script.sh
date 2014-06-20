b76f973723be37b3b104e21f2977b5cb7d61a8ca
Justin Harrison <justin@pyrohail.com>
2014-06-20 07:54:20 -0400

    Add Vagrant development box

---
+++ vagrant/script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+apt-get update
+apt-get upgrade
+
+apt-get install postgresql postgresql-contrib libpq-dev -y # Needed for postgresql
+apt-get install nfs-common portmap -y # Needed for nfs
+apt-get install nodejs -y # Needed for asset pipeline
+apt-get install ruby-dev -y # Needed for rails
+apt-get install build-essential g++ -y # Needed for thin
+
+gem install bundler
+
+sudo -u postgres createuser vagrant --createdb
+
+cd /home/vagrant/code/documentaries
+sudo -u vagrant bundle install
+sudo -u vagrant rake db:create db:migrate
