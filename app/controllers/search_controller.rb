779b8095793e61b95810a5a56aef9f9b04a11a59
Justin Harrison <justin@pyrohail.com>
2014-06-20 21:00:14 -0400

    Cleanup Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,8 @@
 class SearchController < ApplicationController
   def index
-    category_search = params[:search][/"([^"]*)"/]
+    search_array = params[:search].split(',')
+    search_array.each { |v| v.strip! }
 
-    if category_search
-      category_search = category_search.split(':')[1].tr('"', '')
-      without_category = params[:search].slice(category_search)
-      @documentaries = Documentary.where('title like ? or description like' \
-          ' ? or category like ?', "%#{without_category}%", "%#{without_category}%",
-          "%#{category_search}%").paginate(:page => params[:page]).order('total_rating DESC')
-    else
-      @documentaries = Documentary.where('title like ? or description like ?
-          or category like ? ', "%#{params[:search]}%", "%#{params[:search]}%", 
-          "%#{params[:search]}%").paginate(:page => params[:page]).order('total_rating DESC')
-    end
+    @documentaries = Documentary.where('title like ? or category like ?', search_array, search_array).paginate(:page => params[:page]).order('total_rating DESC')
   end
 end
