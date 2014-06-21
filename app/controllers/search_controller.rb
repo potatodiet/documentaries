26d8d82259924d2ede8dd272e10fe6cb94b00ed6
Justin Harrison <justin@pyrohail.com>
2014-06-21 08:33:39 -0400

    Simplify Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,6 @@
 class SearchController < ApplicationController
   def index
-    search_array = params[:search].split(',')
-    search_array.each { |v| v.strip! }
-
-    @documentaries = Documentary.where('title like ? or category like ?', search_array, search_array).paginate(:page => params[:page]).order('total_rating DESC')
+    @documentaries = Documentary.where('title LIKE ? or category LIKE ?', params[:search], params[:search])
+                                .paginate(:page => params[:page]).order('total_rating DESC')
   end
 end
