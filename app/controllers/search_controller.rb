5c9a068ebdb0aa2ec3867c87d6b903ab7856ab10
Justin Harrison <justin@pyrohail.com>
2014-06-24 14:36:01 -0400

    Allow tags with spaces

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   authorize_resource(:class => false)
   
   def index
-    @documentaries = Documentary.tagged_with(params[:search].split(' '))
+    @documentaries = Documentary.tagged_with(params[:search])
                                 .order('created_at DESC')
                                 .paginate(:page => params[:page])
   end
