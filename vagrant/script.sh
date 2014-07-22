659e9a25e6411facec9b6ad23c5c77dd4c4034b2
Justin Harrison <justin@pyrohail.com>
2014-07-22 10:41:16 -0400

    Use rbenv to manage ruby version

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -6,8 +6,19 @@ apt-get upgrade
 apt-get install postgresql postgresql-contrib libpq-dev -y # Needed for postgresql
 apt-get install nfs-common portmap -y # Needed for nfs
 apt-get install nodejs -y # Needed for asset pipeline
-apt-get install ruby-dev -y # Needed for rails
 apt-get install build-essential g++ -y # Needed for native extensions
+apt-get install git -y
+
+# Install rbenv to manage ruby version
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+# Install ruby-build as a rbenv plugin
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+rbenv install 2.1.2
 
 gem install bundler
 
