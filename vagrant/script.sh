61873c8015a56b21fddfe6f2490b62c871d2f2c8
Justin Harrison <justin@pyrohail.com>
2014-07-22 10:21:21 -0400

    Fix documentation error

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -7,7 +7,7 @@ apt-get install postgresql postgresql-contrib libpq-dev -y # Needed for postgres
 apt-get install nfs-common portmap -y # Needed for nfs
 apt-get install nodejs -y # Needed for asset pipeline
 apt-get install ruby-dev -y # Needed for rails
-apt-get install build-essential g++ -y # Needed for thin
+apt-get install build-essential g++ -y # Needed for native extensions
 
 gem install bundler
 
