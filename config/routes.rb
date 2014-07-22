cd2d4aeb82248ac815e90656060225d9b6d3e124
Justin Harrison <justin@pyrohail.com>
2014-07-22 10:19:32 -0400

    Add missing parentheses

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Documentaries::Application.routes.draw do
-  root 'welcome#index'
+  root('welcome#index')
 
-  resources 'reviews', only: ['create']
-  patch 'review' => 'reviews#update', as: 'review_update'
+  resources('reviews', :only => ['create'])
+  patch('review' => 'reviews#update', :as => 'review_update')
 
-  get 'user/view/:id' => 'users#show', as: 'show_user'
-  get 'user/edit' => 'users#edit', as: 'user_edit'
-  patch 'user/edit' => 'users#update'
+  get('user/view/:id' => 'users#show', :as => 'show_user')
+  get('user/edit' => 'users#edit', :as => 'user_edit')
+  patch('user/edit' => 'users#update')
 
-  get 'user/sign_up' => 'users#new', as: 'sign_up'
-  post 'user/sign_up' => 'users#create'
-  get 'user/sign_in' => 'sessions#new', as: 'sign_in'
-  post 'user/sign_in' => 'sessions#create'
-  get 'user/sign_out' => 'sessions#destroy', as: 'sign_out'
+  get('user/sign_up' => 'users#new', :as => 'sign_up')
+  post('user/sign_up' => 'users#create')
+  get('user/sign_in' => 'sessions#new', :as => 'sign_in')
+  post('user/sign_in' => 'sessions#create')
+  get('user/sign_out' => 'sessions#destroy', :as => 'sign_out')
 
-  resources 'search', only: ['index']
-  resources 'documentaries'
+  resources('search', :only => ['index'])
+  resources('documentaries')
 end
