fbfaa00409f0dc959a6125ee3a8b7b44fcf54373
Justin Harrison <justin@pyrohail.com>
2014-07-26 16:28:37 -0400

    Allow mailer to raise errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,5 @@ Documentaries::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 end
