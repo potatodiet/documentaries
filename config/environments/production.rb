58e59a74297208d33910f0cb44c29906d0303210
Justin Harrison <justin@pyrohail.com>
2014-07-26 16:58:59 -0400

    Another possible fix for SMTP auth error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Documentaries::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.mandrillapp.com',
     :port                 => 587,
-    :enable_starttls_auto => true,
+    :enable_starttls_auto => false,
     :user_name            => ENV['MANDRILL_USERNAME'],
     :password             => ENV['MANDRILL_PASSWORD'],
     :authentication       => 'plain',
