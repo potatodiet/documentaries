a6b6ae5a5a5f870bcb5695928689cba7054a7389
Justin Harrison <justin@pyrohail.com>
2014-07-26 16:03:28 -0400

    Add mandrill email support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,13 @@ Documentaries::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'documentaries.pyrohail.com' }
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.mandrillapp.com',
+    :port                 => 25,
+    :enable_starttls_auto => true,
+    :user_name            => ENV['MANDRILL_USERNAME'],
+    :password             => ENV['MANDRILL_PASSWORD'],
+    :authentication       => 'login',
+    :domain               => 'documentaries.pyrohail.com'
+  }
 end
