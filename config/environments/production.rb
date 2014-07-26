72691200db792fad0e9bd978e67cd8d4e2353bf1
Justin Harrison <justin@pyrohail.com>
2014-07-26 16:23:46 -0400

    Another possible fix for SMTP auth error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,9 @@ Documentaries::Application.configure do
   }
 
   config.action_mailer.default_url_options = { :host => 'documentaries.pyrohail.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
