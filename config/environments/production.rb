3f172b0c64d333fd218eb5b83ccaaf6d07c64334
Justin Harrison <justin@pyrohail.com>
2014-07-26 15:50:51 -0400

    Add mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Documentaries::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'documentaries.pyrohail.com' }
 end
