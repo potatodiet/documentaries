6077fa50924dfdff4c464f2597100697aa2615b4
pyrohail <justin@pyrohail.com>
2014-10-01 20:31:12 -0400

    Add more development seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,12 @@
-User.create!(name: "Admin", email: "temp@email.com", password: "Password", role: "admin")
+User.create(name: "Admin", email: "temp@email.com", password: "Password", role: "admin")
+
+if Rails.env.development?
+  sources = %w[D7TB8b2t3QE M7lc1UVf-VE ylLzyHk54Z0 tKTZoB2Vjuk tKTZoB2Vjuk wtLJPvx7-ys 9pmPa_KxsAM bfgO-LXGpTM]
+  8.times do |n|
+    Documentary.create(
+      title: "Documentary_#{n}", description: "Example description",
+      video_source: sources[n], video_service: "youtube", tag_list: "example, testing",
+      uploader_id: 1
+    )
+  end
+end
