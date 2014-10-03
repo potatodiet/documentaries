1e4f34eea9a79cf8cb34d58a152393b32f1aa10d
pyrohail <justin@pyrohail.com>
2014-10-03 03:06:23 -0400

    Accidental duplicate string

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create(name: "Admin", email: "temp@email.com", password: "Password", role: "admin")
 
 if Rails.env.development?
-  sources = %w[D7TB8b2t3QE M7lc1UVf-VE ylLzyHk54Z0 tKTZoB2Vjuk tKTZoB2Vjuk wtLJPvx7-ys 9pmPa_KxsAM bfgO-LXGpTM]
+  sources = %w[D7TB8b2t3QE M7lc1UVf-VE ylLzyHk54Z0 tKTZoB2Vjuk u1zgFlCw8Aw wtLJPvx7-ys 9pmPa_KxsAM bfgO-LXGpTM]
   8.times do |n|
     Documentary.create(
       title: "Documentary_#{n}", description: "Example description",
